cea9a08ed8dafc384f20202b50754871ad1037a7
Peer Mohideen <mpeermohideen90@gmail.com>
2022-05-18 21:50:31 +0400

    1010101/Create-Script56.sql

--- 1010101/Create-Script56.sql
+++ 1010101/Create-Script56.sql
@@ -1,5 +1,5 @@
 CREATE TABLE testdb.persons1(
-    person_id NUMBER(12),
+    person_id NUMBER(14),
     first_name VARCHAR2(50) NOT NULL,
     last_name VARCHAR2(50) NOT NULL,
     PRIMARY KEY(person_id)
